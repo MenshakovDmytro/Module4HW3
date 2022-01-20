b06ca191bca1bd38223a9080a311cbe3136835bd
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-20 10:37:25 +0200

    added Entities

---
+++ Module4HW3/Module4HW3/Data/Entities/TitleEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Module4HW3.Data.Entities
+{
+    public class TitleEntity
+    {
+        public int TitleId { get; set; }
+        public string Name { get; set; }
+        public ICollection<EmployeeEntity> EmployeeEntities { get; set; }
+    }
+}
