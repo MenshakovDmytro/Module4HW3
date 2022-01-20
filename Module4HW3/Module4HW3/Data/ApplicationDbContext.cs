0b22e2798ab911a12b50c3a314b455b7476c90d5
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-20 10:37:52 +0200

    added DbContext

---
+++ Module4HW3/Module4HW3/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module4HW3.Data.Entities;
+using Module4HW3.Data.EntityConfigurations;
+
+namespace Module4HW3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<EmployeeEntity> EmployeeEntities { get; set; }
+        public DbSet<EmployeeProjectEntity> EmployeeProjectEntities { get; set; }
+        public DbSet<OfficeEntity> OfficeEntities { get; set; }
+        public DbSet<ProjectEntity> ProjectEntities { get; set; }
+        public DbSet<TitleEntity> TitleEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleEntityTypeConfiguration());
+        }
+    }
+}
