2793481d8515ec208c054a8ab98d3d1012f6dd22
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-20 10:38:21 +0200

    updated main project

---
+++ Module4HW3/Module4HW3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Module4HW3.Helper;
+
+namespace Module4HW3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
