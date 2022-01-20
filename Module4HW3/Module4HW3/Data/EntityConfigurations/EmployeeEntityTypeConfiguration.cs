9e84b67d6396d76b09b50226a7158af035f23c01
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-20 10:37:41 +0200

    added EntitiesConfigurations

---
+++ Module4HW3/Module4HW3/Data/EntityConfigurations/EmployeeEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Data.Entities;
+
+namespace Module4HW3.Data.EntityConfigurations
+{
+    public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<EmployeeEntity>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeEntity> builder)
+        {
+            builder.ToTable("Employee");
+
+            builder.HasKey(e => e.EmployeeId);
+
+            builder.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.HiredDate).IsRequired().HasMaxLength(7);
+            builder.Property(e => e.DateOfBirth).IsRequired(false);
+            builder.Property(e => e.OfficeId).IsRequired();
+            builder.Property(e => e.TitleId).IsRequired();
+
+            builder.HasOne(o => o.OfficeEntity)
+                .WithMany(e => e.EmployeeEntities)
+                .HasForeignKey(o => o.OfficeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(t => t.TitleEntity)
+                .WithMany(e => e.EmployeeEntities)
+                .HasForeignKey(t => t.TitleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Module4HW3/Module4HW3/Data/EntityConfigurations/TitleEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Data.Entities;
+
+namespace Module4HW3.Data.EntityConfigurations
+{
+    public class TitleEntityTypeConfiguration : IEntityTypeConfiguration<TitleEntity>
+    {
+        public void Configure(EntityTypeBuilder<TitleEntity> builder)
+        {
+            builder.ToTable("Title");
+
+            builder.HasKey(t => t.TitleId);
+
+            builder.Property(t => t.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
