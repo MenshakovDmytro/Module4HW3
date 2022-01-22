23b4f2122b34191dbc270aa1df5d3ec28ffa27f9
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-22 19:33:17 +0200

    added ContextFactory

---
+++ Module4HW3/Module4HW3/ContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Module4HW3.Data;
+
+namespace Module4HW3
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
