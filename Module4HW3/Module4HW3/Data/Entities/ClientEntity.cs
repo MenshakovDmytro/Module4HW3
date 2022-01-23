dcfa2079e573c06b4f2bc4837213d2b0ca6f6507
MenshakovDmytro <jgadf1kz@gmail.com>
2022-01-23 14:33:49 +0200

    added ClientEntity

---
+++ Module4HW3/Module4HW3/Data/Entities/ClientEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module4HW3.Data.Entities
+{
+    public class ClientEntity
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public ICollection<ProjectEntity> ProjectEntities { get; set; }
+    }
+}
